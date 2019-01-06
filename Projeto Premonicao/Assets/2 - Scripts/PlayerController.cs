615576ec00fef6cbc2fa2c903dd4f55773605219
Michelle Santiago <michellesantiagobeadle@hotmail.com>
2019-01-06 21:43:21 -0200

    Movimentação do Player em primeira pessoa

---
+++ Projeto Premonicao/Assets/2 - Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]    // SerialiazeField make it show up in the inspector even though it is a private variable
+    private float speed = 5f;
+    [SerializeField]
+    private float mouseSensitivity = 10f;
+
+    private PlayerMovement movement;
+
+
+    private void Start() {
+
+        // Don't need to check if there is a playermovement because there's a requirement
+        movement = GetComponent<PlayerMovement>();
+    }
+
+
+    private void Update() {
+
+        // 1 - Calculate moment velocity as a 3D vector
+            // Raw don't let unity make their smoothing and processess
+        float horizontalMovement = Input.GetAxisRaw("Horizontal"); // X axis
+        float verticalMovement = Input.GetAxisRaw("Vertical"); // Z axis
+
+            // transform gets the local direction while Vector3.Right gets the global direction
+        Vector3 horizontalDirection = transform.right * horizontalMovement; // (x,0,0)  x = from -1 to 1
+        Vector3 verticalDirection = transform.forward * verticalMovement; // (0,0,z)  z = from -1 to 1
+
+        // - Final movement vector
+            // Normalized make the vector goes to his canonical version
+        Vector3 velocity = (horizontalDirection + verticalDirection).normalized * speed;
+
+        // - Apply movement
+        movement.Move(velocity);
+
+
+        // 2 - calculate rotation as a 3D Vector (turning around)
+        float rotationDirection = Input.GetAxisRaw("Mouse X"); // Y axis
+
+        // - Want to apply the rotation to the camera, not the player, só ir won't mess up with the translation
+        Vector3 rotation = new Vector3(0f, rotationDirection, 0f) * mouseSensitivity;
+
+        // - Apply rotation
+        movement.Rotate(rotation);
+
+
+        // 3 - calculate camera rotation as a 3D Vector (turning around)
+        float cameraRotationDirection = Input.GetAxisRaw("Mouse Y"); // X axis
+
+        Vector3 cameraRotation = new Vector3(cameraRotationDirection, 0f, 0f) * mouseSensitivity;
+
+        // - Apply camera rotation
+        movement.RotateCamera(cameraRotation);
+
+
+    }
+
+}
---
+++ Projeto Premonicao/Assets/2 - Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement: MonoBehaviour {
+
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
+
+    private Rigidbody rb;
+
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Run avery physics iteration
+    private void FixedUpdate() {
+        PerformMovement();
+        PerformRotation();
+    }
+
+
+    // Gets a movement vector
+    public void Move(Vector3 _velocity) {
+        velocity = _velocity;
+    }
+
+    // Gets a rotation vector
+    public void Rotate(Vector3 _rotation) {
+        rotation = _rotation;
+    }
+
+    // Gets a rotation vector
+    public void RotateCamera(Vector3 _cameraRotation) {
+        cameraRotation = _cameraRotation;
+    }
+
+    // Perform movement based on velocity variable
+    private void PerformMovement() {
+
+        if (velocity != Vector3.zero) {
+
+            // different from transform.MovePosition because if it's something on they way we are moving it will not transpass - does the physics checks
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+
+    }
+
+    // Perform rotation based on mouseSentivity variable
+    private void PerformRotation() {
+
+        // Quaternions fazem a rotação então não são Vector3's - Quaternion.Euler transforms that Vector3 rotation into a Quaternion
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+
+        if (cam != null) {
+            cam.transform.Rotate(-cameraRotation);
+        }
+
+    }
+
+
+}
