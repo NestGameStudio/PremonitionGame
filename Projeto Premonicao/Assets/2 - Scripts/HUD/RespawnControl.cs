c1ca903dda91423aa99491dd8bf4f4490c63a0a5
Michelle Santiago <michellesantiagobeadle@hotmail.com>
2019-01-17 18:12:09 -0200

    Respawn

---
+++ Projeto Premonicao/Assets/2 - Scripts/HUD/RespawnControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RespawnControl : MonoBehaviour
+{
+    private Vector3 respawnPosition;
+    private Quaternion respawnRotation;
+    private float time;
+
+    public Text TimeRegression;
+    public float ExplosionTimer = 60f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawnPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        respawnRotation = GameObject.FindGameObjectWithTag("Player").transform.rotation;
+
+        StartCoroutine(ExplosionTimeRegression());
+    }
+
+    IEnumerator ExplosionTimeRegression() {
+
+        while (true) {
+
+            if (time <= ExplosionTimer) {
+
+                yield return new WaitForSecondsRealtime(1);
+                time += 1;
+                if (ExplosionTimer - time >= 0)
+                    TimeRegression.text = (ExplosionTimer - time).ToString();
+
+            } else {
+                Application.LoadLevel(Application.loadedLevel);
+                break;
+            }
+
+        }
+
+    }
+
+}
