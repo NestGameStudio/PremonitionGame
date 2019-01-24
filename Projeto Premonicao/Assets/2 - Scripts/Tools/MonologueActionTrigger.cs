c8102f568ed576420f93f8096a0eb6411658d59c
Michelle Santiago <michellesantiagobeadle@hotmail.com>
2019-01-24 12:25:32 -0200

    As portas tem mensagens de erro

---
+++ Projeto Premonicao/Assets/2 - Scripts/Tools/MonologueActionTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonologueActionTrigger : MonoBehaviour
+{
+    public void PlayAction() {
+        if (this.GetComponent<ActionTrigger>() != null)
+            this.GetComponent<ActionTrigger>().DoAction();
+        else
+            Debug.Log("Não tem action saporra");
+    }
+}
