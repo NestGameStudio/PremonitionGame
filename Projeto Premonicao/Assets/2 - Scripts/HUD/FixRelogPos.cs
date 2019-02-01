72f04e48e9dee2c015f6075cbdc6b458652e9c34
lucas3311 <lucaslimareis3311@gmail.com>
2019-02-01 20:58:38 -0200

    Fix

---
+++ Projeto Premonicao/Assets/2 - Scripts/HUD/FixRelogPos.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FixRelogPos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<RectTransform>().localPosition = new Vector3(0,0,0);   
+    }
+}
