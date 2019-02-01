cae314bbdfbe9da2d7ece5053d0b16e896582bcb
lucas3311 <lucaslimareis3311@gmail.com>
2019-02-01 20:51:04 -0200

    UI extensions  + relogin com várias telas

---
+++ Projeto Premonicao/Assets/2 - Scripts/HUD/ScrollWatch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollWatch : MonoBehaviour
+{
+    private float actualPosition = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<ScrollRect>().horizontalNormalizedPosition = actualPosition;
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f && actualPosition != 1) // forward
+        {
+            actualPosition += 0.5f;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f && actualPosition != 0) // backwards
+        {
+            actualPosition -= 0.5f;
+        }
+    }
+}
