e2213956020194d23618e0f951f418d319778fb6
lucas3311 <lucaslimareis3311@gmail.com>
2019-01-31 22:59:03 -0200

    Bagageiro

---
+++ Projeto Premonicao/Assets/2 - Scripts/Actions/OpenBagageiro.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenBagageiro : MonoBehaviour
+{
+    private bool open;
+    // Start is called before the first frame update
+    void OnMouseDown()
+    {
+        open = !open;
+        gameObject.GetComponent<Animator>().SetBool("Open", open);
+    }
+}
