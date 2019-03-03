641d30e5563ac4be3d849f90be9575f81c4d5eab
Michelle Santiago <michellesantiagobeadle@hotmail.com>
2019-03-03 12:40:47 -0300

    mini state machine

--- Projeto Premonicao/Assets/2 - Scripts/HUD/ScrollWatch.cs
+++ Projeto Premonicao/Assets/2 - Scripts/HUD/ScrollWatch.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class ScrollWatch : MonoBehaviour
 {
     private float actualPosition = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,10 +19,12 @@ public class ScrollWatch : MonoBehaviour
         gameObject.GetComponent<ScrollRect>().horizontalNormalizedPosition = actualPosition;
         if (Input.GetAxis("Mouse ScrollWheel") > 0f && actualPosition != 1) // forward
         {
+            TimeSceneController.Instance.ChangeSceneState(true);
             actualPosition += 0.5f;
         }
         if (Input.GetAxis("Mouse ScrollWheel") < 0f && actualPosition != 0) // backwards
         {
+            TimeSceneController.Instance.ChangeSceneState(false);
             actualPosition -= 0.5f;
         }
     }
---
+++ Projeto Premonicao/Assets/2 - Scripts/StateMachine/TimeSceneController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeSceneController : MonoBehaviour
+{
+    private static TimeSceneController instance;
+
+    [Range(1, 5)]
+    private int currentState = 1;
+
+    public static TimeSceneController Instance { get { return instance; } }
+
+    public void Awake() {
+
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+        } else {
+            instance = this;
+        }
+
+    }
+
+    public void ChangeSceneState(bool increase) {
+
+        if (increase) {
+
+            if (currentState != 5)
+                currentState += 1;
+            Debug.Log("Adiantou uma cena " + currentState);
+        } else {
+
+            if (currentState != 1)
+                currentState -= 1;
+            Debug.Log("Voltou uma cena " + currentState);
+        }
+
+    }
+
+    private void Update() {
+        
+        switch (currentState) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+                break;
+        }
+
+    }
+
+}
