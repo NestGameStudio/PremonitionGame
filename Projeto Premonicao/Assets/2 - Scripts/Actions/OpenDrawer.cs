b7330fdd4aa778e1c7c6551c6eab9aa2f2dc931f
lucas3311 <lucaslimareis3311@gmail.com>
2019-03-04 20:58:58 -0300

    Abrir bagageiro

---
+++ Projeto Premonicao/Assets/2 - Scripts/Actions/OpenDrawer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDrawer : ActionTrigger
+{
+    public bool isOpen = false;
+    public override void DoAction()
+    {
+        base.DoAction();
+        gameObject.GetComponent<Animator>().SetBool("Open", isOpen);
+        isOpen = !isOpen;
+    }
+}
