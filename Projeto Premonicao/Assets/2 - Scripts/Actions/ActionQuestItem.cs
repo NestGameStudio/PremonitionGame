1ce4b1eefbf74c38e3dfd6abcbfb9391c6553308
Michelle Santiago <michellesantiagobeadle@hotmail.com>
2019-03-04 21:42:13 -0300

    trocaa posicao de acordo com la quest

--- Projeto Premonicao/Assets/2 - Scripts/Actions/ActionQuestItem.cs
+++ Projeto Premonicao/Assets/2 - Scripts/Actions/ActionQuestItem.cs
@@ -6,9 +6,17 @@ public class ActionQuestItem : ActionInteraction {
 
     public GameObject rewardObject;
 
+    [Header("Cena 1")]
     public bool ActiveInScene1 = true;
+    public Vector3 NewPosition1;
+
+    [Header("Cena 2")]
     public bool ActiveInScene2 = true;
+    public Vector3 NewPosition2;
+
+    [Header("Cena 3")]
     public bool ActiveInScene3 = true;
+    public Vector3 NewPosition3;
 
     // parar de fazer ele multiplicar cartôes infinitos e selecionar qual cena que se pode fazer essa quest
 
@@ -20,6 +28,9 @@ public class ActionQuestItem : ActionInteraction {
             if ((TimeSceneController.Instance.currentState == 1 && ActiveInScene1) || (TimeSceneController.Instance.currentState == 2 && ActiveInScene2) || (TimeSceneController.Instance.currentState == 3 && ActiveInScene3)) {
                 InventoryManager.Instance.removeObjectFromInventory(TriggerObject);
                 rewardObject.SetActive(true);
+                this.GetComponent<TimeSceneNPC>().PositionScene1 = NewPosition1;
+                this.GetComponent<TimeSceneNPC>().PositionScene2 = NewPosition2;
+                this.GetComponent<TimeSceneNPC>().PositionScene3 = NewPosition3;
             } else {
                 Debug.Log("Quest não está ativa");
             }
